10bb9f1f33af6abb5c97b14e0c78f05f0ad134a5
Marcos Duarte <marcosduarte1994@gmail.com>
2022-12-07 20:39:03 -0300

    feat: add outlet from react-router-dom

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 import { History } from './pages/history'
 import { Home } from './pages/home'
@@ -6,8 +7,10 @@ import { Home } from './pages/home'
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
