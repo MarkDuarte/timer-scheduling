ac14e9bcb0358d695c35bc8a912aee2b51c6a473
Marcos Duarte <marcosduarte1994@gmail.com>
2022-12-19 23:03:36 -0300

    feat: style placeholder of inputs

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -36,10 +36,25 @@ const BaseInput = styled.input`
   font-weight: bold;
   font-size: 1.125rem;
   padding: 0 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme['green-500']};
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
 `
 
-export const TaskInput = styled(BaseInput)``
-export const MinutesAmountInput = styled(BaseInput)``
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+`
+
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`
 
 export const CountdownContainer = styled.div`
   font-family: 'Roboto Mono', monospace;
