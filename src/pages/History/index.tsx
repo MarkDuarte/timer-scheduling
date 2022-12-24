92e354cec5502e2bd6eb12477773516e6ffe4d2b
Marcos Duarte <marcosduarte1994@gmail.com>
2022-12-23 22:47:12 -0300

    feat: create new styled in table and status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from '../History/styles'
+import { HistoryContainer, HistoryList, Status } from '../History/styles'
 
 export function History() {
   return (
@@ -21,31 +21,41 @@ export function History() {
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Ha 2 meses</td>
-              <td>Concluido</td>
+              <td>
+                <Status statusColor="green">Concluída</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Ha 2 meses</td>
-              <td>Concluido</td>
+              <td>
+                <Status statusColor="green">Concluída</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Ha 2 meses</td>
-              <td>Concluido</td>
+              <td>
+                <Status statusColor="green">Concluída</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Ha 2 meses</td>
-              <td>Concluido</td>
+              <td>
+                <Status statusColor="yellow">Em Andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Ha 2 meses</td>
-              <td>Concluido</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
           </tbody>
         </table>
